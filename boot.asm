0bbb8ad00be124069ee0cb512a2597d506e2dd45
Darilrodriguez <darilrodriguez.2@gmail.com>
2015-10-26 01:35:56 -0300

    Create boot.asm

---
+++ boot.asm
@@ -0,0 +1,31 @@
+use16
+org 0x7C00
+
+_start:
+        jmp _loadkernel
+
+_loadkernel:
+        mov dl, 00h
+        mov dh, 00h
+        mov ch, 00h
+        mov dh, 02h
+        mov bx, 1000h
+        mov es, bx
+        mov bx, 00h
+
+        mov ah, 02h
+        mov al, 01h
+        int 13
+        jc _loadkernel
+
+        mov ax, 1000h
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov ss, ax
+
+        jmp 1000h:00h
+
+times 510-($-$$) db 0
+dw 0xAA55
