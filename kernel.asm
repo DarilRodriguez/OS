b33280cd24799eb9c1ab3847e673e52ced617513
Darilrodriguez <darilrodriguez.2@gmail.com>
2015-10-27 00:53:23 -0300

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -1,5 +1,3 @@
-org 1000h
-
 
 _main:
         ;==init==
@@ -10,8 +8,6 @@ _main:
         mov ah, 00h
         mov al, 03h
         int 10h
-
-        jmp _readDisk
 
         jmp .prompt
         .loop:
@@ -47,11 +43,7 @@ _main:
                         mov byte [buffer_pos], ah
                         jmp .end
                 .ret:
-                        mov ah, 0Eh
-                        mov al, 0Dh
-                        int 10h
-                        mov al, 0Ah
-                        int 10h
+                        call print_endl
                         jmp .endcode
 
                 .end:
@@ -60,19 +52,10 @@ _main:
                 .endcode:
 
                 ;==code==
-                cmp [buffer_pos], 00h
-                je .prompt
-                mov ecx, 00h
-                mov ah, 0Eh
-                .looPcode:
 
-                mov al, [text_buffer+ecx]
-                int 10h
-
-                inc ecx
-                cmp cl, byte [buffer_pos]
-                jne .looPcode
-                mov [buffer_pos], 00h
+                mov eax, [buffer_pos]
+                call printhex
+                call code
 
                 ;========
 
@@ -84,14 +67,38 @@ _main:
                 int 10h
                 jmp .loop
 
-print:
-        mov dx, 0
-        mov ah, 0Eh
-        .loop:
-        lodsb
-        cmp al, 00h
+code:
+        cmp [buffer_pos], 00h
         je .end
-        int 10h
+
+        cmp [buffer_pos], 04h
+        jnge .nex1
+             cmp [text_buffer], 's'
+             jne .nex1
+             cmp [text_buffer+1], 't'
+             jne .nex1
+             cmp [text_buffer+2], 'd'
+             jne .nex1
+             cmp [text_buffer+3], 'w'
+             jne .nex1
+             mov ax, 5307h
+             mov cx, 0003h
+             int 15h
+        .nex1:
+
+        .end:
+        mov [buffer_pos], 00h
+        ret
+
+print:
+        .loop:
+                mov al, [si]
+                cmp al, 00h
+                je .end
+                mov ah, 0Eh
+                int 10h
+                inc si
+                jmp .loop
         .end:
         ret
 
@@ -102,21 +109,57 @@ printc:
         int 10h
         ret
 
-_readDisk:
+printhex:
+        mov dl, al
+        shr al, 4
+        shl dl, 4
+        shr dl, 4
 
-        mov ah, 02h
-        mov al, 01h
-        mov ch, 00h
-        mov cl, 02h
-        mov dh, 01h
-        mov dl, 80h
-        mov bx, 800h
-        int 13h
+        cmp al, 0Ah
+        jl .lowa
+        add al, 55
+        jmp .enda
+        .lowa:
+        add al, 48
+        .enda:
+        mov ah, 0Eh
+        int 10h
 
-        mov al, [800h]
-        call printc
+        mov al, dl
+        cmp al, 0Ah
+        jl .lowb
+        add al, 55
+        jmp .endb
+        .lowb:
+        add al, 48
+        .endb:
+        mov ah, 0Eh
+        int 10h
+        ret
 
-        jmp $
+print_endl:
+        mov ah, 0Eh
+        mov al, 0Dh
+        int 10h
+        mov al, 0Ah
+        int 10h
+        ret
+
+;_readDisk:
+;
+;        mov ah, 02h
+;        mov al, 01h
+;        mov ch, 00h
+;        mov cl, 02h
+;        mov dh, 01h
+;        mov dl, 80h
+;        mov bx, 800h
+;        int 13h
+;
+;        mov al, [800h]
+;        call printc
+;
+;        jmp $
 
 buffer_pos dd 00h
 text_buffer db 00h
