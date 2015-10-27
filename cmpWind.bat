134a41735b8fa6381239406582fe0594927f3e02
Darilrodriguez <darilrodriguez.2@gmail.com>
2015-10-27 00:53:58 -0300

    Update cmpWind.bat

--- cmpWind.bat
+++ cmpWind.bat
@@ -2,8 +2,8 @@
 fasm bootloader.asm
 fasm kernel.asm
 dd if=kernel.bin of=./cdiso/boot.flp
-dd if=bootloader.bin of=./cdiso/kernel.flp
+dd if=boot.bin of=./cdiso/kernel.flp
 mkisofs -no-emul-boot -boot-load-size 4 -o OS.iso -b boot.flp cdiso/ cdiso/kernel.flp 
 
-del bootloader.bin
+del boot.bin
 del kernel.bin
