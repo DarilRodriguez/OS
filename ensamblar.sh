c0e3c8e84463f4d90312bc72a9eb156faf88b5b6
Darilrodriguez <darilrodriguez.2@gmail.com>
2015-11-06 11:39:01 -0300

    Update ensamblar.sh

--- ensamblar.sh
+++ ensamblar.sh
@@ -2,8 +2,8 @@
 
 fasm boot.asm boot.bin
 fasm kernel.asm kernel.bin
-dd if=kernel.bin of=./cdiso/boot.flp
-dd if=boot.bin of=./cdiso/kernel.flp
+dd if=kernel.bin of=./cdiso/kernel.flp
+dd if=boot.bin of=./cdiso/boot.flp
 mkisofs -no-emul-boot -boot-load-size 4 -o OS.iso -b boot.flp cdiso/ cdiso/kernel.flp 
 qemu-system-x86_64 -cdrom OS.iso
 
